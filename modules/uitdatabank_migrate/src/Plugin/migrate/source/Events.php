d92e3115c560b5e83c816a814fc5949fda70c603
Andreas De Rijcke <me@andreasderijcke.be>
2017-12-11 15:48:31 +0100

    Updated Event migrate.

--- modules/uitdatabank_migrate/src/Plugin/migrate/source/Events.php
+++ modules/uitdatabank_migrate/src/Plugin/migrate/source/Events.php
@@ -19,9 +19,13 @@ class Events extends Url {
    * {@inheritdoc}
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+    $params = '?start=4&limit=1&embed=true&apiKey=270cbec1-d89d-43e5-9bac-94ca0757c524';
 
-    // @todo: add query parameters to url.
+    if (!is_array($configuration['urls'])) {
+      $configuration['urls'] = [$configuration['urls'] . $params];
+    }
+
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
   }
 
   /**
@@ -29,7 +33,35 @@ class Events extends Url {
    */
   public function prepareRow(Row $row) {
 
-    // todo: custom stuff.
+    // @todo: make plugin for this.
+    // Handle translatable fields, also need to catch language not present
+    // source.
+    $translatable_fields = [
+      'address',
+      'description',
+      'title',
+    ];
+    $langcode = $row->getSourceProperty('language');
+    $langcode = $langcode?: 'nl';
+    foreach ($translatable_fields as $name) {
+      $value = $row->getSourceProperty($name);
+
+      if (isset($value[$langcode])) {
+        $value = $value[$langcode];
+        $row->setSourceProperty($name, $value);
+      }
+    }
+    unset($name, $value, $translatable_fields);
+
+    // Preprocess typicalAgeRange values.
+    $value = $row->getSourceProperty('age_min');
+    $ages = explode('-', $value);
+    foreach ($ages as $index => $age) {
+      $ages[$index] = (int) $age;
+    }
+    $row->setSourceProperty('age_min', isset($ages[0]) ? $ages[0] : 0);
+    $row->setSourceProperty('age_max', isset($ages[1]) ? $ages[1] : NULL);
+    unset($age, $ages, $index, $value);
 
     return parent::prepareRow($row);
   }
