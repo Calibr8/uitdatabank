ccdabcacb5f760e9da3ae7a20b2270817e5e0176
Andreas De Rijcke <me@andreasderijcke.be>
2017-12-19 14:36:59 +0100

    Added processing of openingHours and priceInfo in Events and Places migration.

---
+++ modules/uitdatabank_migrate/src/Plugin/migrate/process/UitdatabankPriceInfo.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\uitdatabank_migrate\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Maps UiTdatabank price info data to uitdatabank_price_info field.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "uitdatabank_price_info",
+ *   handle_multiples = TRUE
+ * )
+ */
+class UitdatabankPriceInfo extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $parsed = [];
+
+    foreach ($value as $index => $item) {
+      $parsed[$index] = [
+        'category' => $item['category'],
+        'name' => $item['name'],
+        'price' => $item['price'],
+        'price_currency' => $item['priceCurrency'],
+      ];
+    }
+
+    return $parsed;
+  }
+
+}
