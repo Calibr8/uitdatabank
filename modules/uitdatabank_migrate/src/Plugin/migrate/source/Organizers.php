584c64222d91c6010b485a86ae96852b1bf1ff7b
Andreas De Rijcke <me@andreasderijcke.be>
2018-02-20 14:44:48 +0100

    Fix for organiser name values.

--- modules/uitdatabank_migrate/src/Plugin/migrate/source/Organizers.php
+++ modules/uitdatabank_migrate/src/Plugin/migrate/source/Organizers.php
@@ -3,6 +3,7 @@
 namespace Drupal\uitdatabank_migrate\Plugin\migrate\source;
 
 use Drupal\migrate\Plugin\MigrationInterface;
+use Drupal\migrate\Row;
 use Drupal\migrate_plus\Plugin\migrate\source\Url;
 use Drupal\uitdatabank_migrate\Utility\UitdatabankMigrateHelper;
 
@@ -22,6 +23,31 @@ class Organizers extends Url {
 
     $configuration = UitdatabankMigrateHelper::addEndpointParameters($configuration, 'organizers');
     parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+
+    // Handle translatable fields, also need to catch language not present
+    // source.
+    $translatable_fields = [
+      'name',
+    ];
+    $langcode = $row->getSourceProperty('language');
+    $langcode = $langcode?: 'nl';
+    foreach ($translatable_fields as $name) {
+      $value = $row->getSourceProperty($name);
+
+      if (isset($value[$langcode])) {
+        $value = $value[$langcode];
+        $row->setSourceProperty($name, $value);
+      }
+    }
+    unset($name, $value, $translatable_fields);
+
+    return parent::prepareRow($row);
   }
 
 }
